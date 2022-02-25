9e5543851ea6cb3ac8cd4b99d19a80ddc9405ae2
FritzWilli <kirschbluetenbau@gmail.com>
2022-02-25 21:50:31 +0100

    Add project files.

---
+++ Technik-Water-Project.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Technik_Water_Project.ino
+ Created:	2/25/2022 9:49:36 PM
+ Author:	Vitas Ruhland
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
